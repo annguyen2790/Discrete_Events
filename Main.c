67f7cb49e98dd71c42ec3cda8fce40a9d13a0c47
An Nguyen <tuh09895@temple.edu>
2019-08-28 11:24:00 -0400

    Second Commit

--- Main.c
+++ Main.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "process_struct.h"
 
+
+/*Global Variables  */
+int INIT_TIME = 0;
+int FIN_TIME;
+int ARRIVE_MIN;
+int ARRIVE_MAX;
+int QUIT_PROB;
+int CPU_MIN;
+int CPU_MAX;
+int DISK1_MIN;
+int DISK1_MAX;
+int DISK2_MIN;
+int DISK2_MAX;
+
+/*END OF GLOBAL VARIABLES */
 int main(void){
   puts("Hello World");
 }
