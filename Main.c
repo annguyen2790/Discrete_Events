2a7de5ec1e326d3b082bce474a501e0b64923340
An Nguyen <tuh09895@temple.edu>
2019-09-07 23:35:54 -0400

    Update event structure for better code readibility

--- Main.c
+++ Main.c
@@ -2,19 +2,6 @@
 #include <stdio.h>
 #include "process_struct.h"
 #include <string.h>
-/*Global Variables  */
-int INIT_TIME = 0;
-int FIN_TIME = 1000;
-int ARRIVE_MIN;
-int ARRIVE_MAX;
-int QUIT_PROB;
-int CPU_MIN = 0;
-int CPU_MAX;
-int DISK1_MIN;
-int DISK1_MAX;
-int DISK2_MIN;
-int DISK2_MAX;
-/*END OF GLOBAL VARIABLES */
 
 /*Open/Close state of CPU, DISK 1, and DISK 2
 At the beginning of the program, all are opened  */
@@ -22,10 +9,7 @@ int CPU_OPEN = 1;
 int DISK_1_OPEN = 1;
 int DIS_2_OPEN = 1;
 
-
 //State of jobs
-
-
 int stimulation_ended = 0;
 int entered_disk_1 =  1;
 int entered_disk_2 = 2;
@@ -41,18 +25,20 @@ Queue * init_queue(){ /*Create a queuse / pointer to Queue struct */
   s_queue ->tail = NULL;
   return s_queue;
 }
-void insert_Pqueue(Queue * q, int job_ID, int job_state, int job_time){ /*This method insert Job in a queue with a priority based on the time of the job*/
+
+
+void insert_Pqueue(Queue * q, Job * event){ /*This method insert Job in a queue with a priority based on the time of the job*/
   Job * temp = malloc(sizeof(Job));
   Job * temp2 = malloc(sizeof(Job));
-  temp->ID = job_ID;
-  temp->state = job_state;
-  temp->time = job_time;
+  temp->ID = event->ID;
+  temp->state = event->state;
+  temp->time = event->time;
   if(q -> head == NULL ||temp->time  <  q->head->time){
     temp->nextPtr = q->head;
     q->head = temp;   
   }else{
     temp2 = q->head;
-    while(temp2->nextPtr != NULL && temp2->nextPtr->time <= job_time){
+    while(temp2->nextPtr != NULL && temp2->nextPtr->time <= event->time){
       temp2 = temp2->nextPtr;
     }
     temp->nextPtr = temp2->nextPtr;
@@ -61,11 +47,11 @@ void insert_Pqueue(Queue * q, int job_ID, int job_state, int job_time){ /*This m
   q->size++;
   
 }
-void insert_queue(Queue * q, int job_ID, int job_state, int job_time){ /*This method insert a Job at the tail of the queue*/
+void insert_queue(Queue * q, Job * event){ /*This method insert a Job at the tail of the queue*/
   Job * temp = malloc(sizeof(Job));
-  temp->ID = job_ID;
-  temp->state = job_state;
-  temp->time = job_time;
+  temp->ID = event->ID;
+  temp->state = event->state;
+  temp->time = event->time;
   temp->nextPtr = NULL;
   
   if(q->head == NULL){
@@ -118,9 +104,8 @@ float read_inputs(char *file_name, char *string){ /*This method will read and se
   char temp[200];         
   char string_holder[200]; 
   float value = 0;
-  int i = 0;
-  int j = 0;
-    
+  size_t i = 0;
+  size_t j = 0;
   if((filePtr = fopen(file_name, "r")) == NULL){
     return(0);
   }
@@ -130,23 +115,50 @@ float read_inputs(char *file_name, char *string){ /*This method will read and se
 	string_holder[j] = temp[i];
 	j++;
       }
-      value = atof(string_holder);
-      
-            
-    }
-        
+      value = atof(string_holder);          
+    }        
   }
   fclose(filePtr);
-  return value;
-    
+  return value;    
+}
+Job * create_job(int job_id, int job_state, int job_time){
+  Job * temp = malloc(sizeof(Job));
+  temp->ID = job_id;
+  temp->state = job_state;
+  temp->time = job_time;
+  return temp;
 }
  int main(void){
-
+   //Read in the inputs from the inputs.txt
+  float SEED  = read_inputs("inputs.txt", "SEED"); 
+  float INIT_TIME = read_inputs("inputs.txt","INIT_TIME");
+  float FIN_TIME = read_inputs("inputs.txt","FIN_TIME");
+  float ARRIVE_MIN = read_inputs("inputs.txt","ARRIVE_MIN");
+  float ARRIVE_MAX = read_inputs("inputs.txt","ARRIVE_MAX");
+  float QUIT_PROB = read_inputs("inputs.txt","QUIT_PROB");
+  float CPU_MIN = read_inputs("inputs.txt","CPU_MIN");
+  float CPU_MAX = read_inputs("inputs.txt","CPU_MAX");
+  float DISK1_MIN = read_inputs("inputs.txt","DISK1_MIN");
+  float DISK1_MAX = read_inputs("inputs.txt","DISK1_MAX");
+  float DISK2_MIN = read_inputs("inputs.txt","DISK2_MIN");
+  float DISK2_MAX = read_inputs("inputs.txt","DISK2_MAX");
   //Create Four Main Queues Data Structures 
   Queue * CPU_queue = init_queue();
   Queue * DISK_1_queue = init_queue();
   Queue * DISK_2_queue = init_queue();
   Queue * EventQueue = init_queue();
-  float check = read_inputs("inputs.txt", "QUIT_PROB");
-  printf("%f\n", check);
+  /*
+``1. Create the first job
+  2. Add a job into the event Queue
+  3. 
+   */
+  Job * job1 = create_job(1,1,0);
+  Job * job2 = create_job(1,1,2);
+  Job * job3 = create_job(1,1,3);
+  Job * job4 = create_job(1,1,4);
+  insert_queue(EventQueue, job1);
+  insert_Pqueue(EventQueue, job4);
+  insert_Pqueue(EventQueue, job3);
+  insert_Pqueue(EventQueue, job2);
+  print_queue(EventQueue);
 }
