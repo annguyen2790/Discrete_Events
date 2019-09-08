b399fdd94659132e9ed70b3c680884489ce66b69
An Nguyen <tuh09895@temple.edu>
2019-09-08 02:09:40 -0400

    Update Prototypes

--- process_struct.h
+++ process_struct.h
@@ -19,3 +19,5 @@ Job * delete_head(Queue * q);
 void insert_Pqueue(Queue * q, Job * event);
 void destroy_queue(Queue * q);
 float read_inputs(char * filename, char * string);
+int get_random(int high, int low);
+void enter_disk(Queue * disk_Queue, Queue * event, int disk, int time);
